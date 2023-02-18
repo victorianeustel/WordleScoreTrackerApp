2aa08e9073e8b867c63b935010db7fc5d78baff9
victorianeustel <100964778+victorianeustel@users.noreply.github.com>
2023-02-17 23:56:04 -0600

    Queries Created / Updated

--- server/queries.js
+++ server/queries.js
@@ -18,6 +18,15 @@ const getWords = (request, response) => {
     })
 }
 
+const getScores = (request, response) => {
+    pool.query('SELECT * FROM scores order by word_id ASC, user_id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
       if (error) {
@@ -38,8 +47,21 @@ const getUserById = (request, response) => {
     })
   }
 
+  const getScoresByWordID = (request, response) => {
+    const id = partInt(request.params.id)
+
+    pool.query('select words.word_id, users.user_id, score_value from scores full join users on scores.user_id = users.user_id full join words on scores.word_id = words.word_id where words.word_id = $1', [id], (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).json(results.rows)
+      })
+  }
+
   module.exports = {
     getWords,
     getUsers,
     getUserById,
+    getScores,
+    getScoresByWordID,
   }
