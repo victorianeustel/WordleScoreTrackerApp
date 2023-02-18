5ccd30ec8f795576eb1afaf14a743ae3bb0d7533
victorianeustel <100964778+victorianeustel@users.noreply.github.com>
2023-02-17 19:39:04 -0600

    Server Creation

---
+++ server/queries.js
@@ -0,0 +1,45 @@
+//pkill -f node
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: '35.193.9.16',
+  database: 'postgres',
+  password: 'sDf%QpOPP:HI,q4R',
+  port: 5432,
+})
+
+const getWords = (request, response) => {
+    pool.query('SELECT * FROM words ORDER BY word_id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM users WHERE user_id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+    getWords,
+    getUsers,
+    getUserById,
+  }
--- src/app/Models/score.ts
+++
@@ -1,6 +0,0 @@
-
-export interface Score {
-    userID: number;
-    wordID: number;
-    score: number;
-}
