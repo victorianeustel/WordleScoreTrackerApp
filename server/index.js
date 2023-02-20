8e841a7edde93bb0c194e51498d441d874766577
victorianeustel <100964778+victorianeustel@users.noreply.github.com>
2023-02-20 13:22:28 -0600

    Server Changes

--- server/index.js
+++ server/index.js
@@ -1,102 +1,79 @@
-//pkill -f node
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const port = process.env.PORT || 8080;
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: '35.193.9.16',
-  database: 'postgres',
-  password: 'sDf%QpOPP:HI,q4R',
-  port: 5432,
+  user: process.env.DB_USER = 'postgres',
+  password: process.env.DB_PASS = 'lauren97',
+  database: process.env.DB_NAME = 'wordle-contest',
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
 })
 
-const getWords = (request, response) => {
-    pool.query('SELECT * FROM words ORDER BY word_id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-}
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-const getScores = (request, response) => {
-    pool.query('SELECT * FROM scores order by word_id ASC, user_id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-}
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
 
-const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-}
-
-const getUserById = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    pool.query('SELECT * FROM users WHERE user_id = $1', [id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
-
-  const getScoresByWordID = (request, response) => {
-    const id = parseInt(request.params.id)
-
-    pool.query('SELECT user_id, score_value FROM scores WHERE word_id = $1', [id], (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-  }
-
-  const getRankings = (request, response) => {
-    pool.query('SELECT * FROM public."rankingByAverage"', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
-
-  const addWord = (request, response) => {
-    const { word_value, word_date } = request.body
-
-    pool.query('Insert into words (word_value, word_date) values ($1, $2) RETURNING *', [word_value, word_date], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`Word added with ID: ${results.rows[0].id}`)
-    })
-}
-
-const addScore = (request, response) => {
-  const { user_id, word_id, score_value } = request.body
-
-  pool.query('Insert into scores (user_id, word_id, score_value) values ($1, $2, $3) RETURNING *', [user_id, word_id, score_value], (error, results) => {
+app.get('/users', async (request, response)=>
+  pool.query('SELECT * FROM users ORDER BY user_id ASC', (error, results) => {
     if (error) {
       throw error
     }
-    response.status(201).send(`Score added with ID`)
+    response.status(200).json(results.rows)
   })
-}
+)
 
-  module.exports = {
-    getWords,
-    getUsers,
-    getUserById,
-    getScores,
-    getScoresByWordID,
-    getRankings,
-    addWord,
-    addScore,
+app.get('/scores', async (request, response)=>
+pool.query('SELECT * FROM scores order by word_id ASC, user_id ASC', (error, results) => {
+  if (error) {
+    throw error
   }
+  response.status(200).json(results.rows)
+})
+)
 
+app.get('/words', async (request, response)=>
+  pool.query('SELECT * FROM words ORDER BY word_id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+
+)
+
+// app.get('/words', db.getWords)
+
+// app.get('/users/:id', db.getUserById)
+
+// app.get('/scores', db.getScores)
+
+// app.get('/scores/:id', db.getScoresByWordID)
+
+// app.get('/rankings', db.getRankings)
+
+
+
+// app.get('/addScores', db.addScore)
+
+// app.get('/addWord', db.addWord)
+
+  
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
+// Listen to the specified port, otherwise 3080
+const PORT = process.env.PORT || 8090;
+const server = app.listen(PORT, () => {
+  console.log(`Server Running: http://localhost:${PORT}`);
+});
